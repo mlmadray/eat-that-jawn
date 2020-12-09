a97e6f911ddf7d0e15795b6efc22229a4a09b53e
Dennis Yoon <dhy6785@gmail.com>
2020-12-08 19:00:35 -0500

    dennis 2

--- vue/src/main.js
+++ vue/src/main.js
@@ -8,9 +8,22 @@ Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
 
+Vue.prototype.$http = axios;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  data: {
+    restaurants: []
+  },
+  render: h => h(App),
+  created() {
+    var vm = this
+    this.$http
+      .get('https://developers.zomato.com/api/v2.1/search?entity_id=287&entity_type=city&apikey=565aaf5d257ecac2fb2db7425a942cc0')
+      .then(function(response) {
+        vm.restaurants = response.data
+      })
+  }
+},).$mount('#app')
+
